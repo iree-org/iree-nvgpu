241fed6d34d7f0bfb469ec232b990504919d2e1b
Eugene Zhulenev <ezhulenev@google.com>
2023-05-21 12:39:07 -0700

    [cuBLAS] Add cublas.gemm operation

--- compiler/src/openxla/compiler/nvgpu/Dialect/CUBLAS/IR/CUBLASOps.cpp
+++ compiler/src/openxla/compiler/nvgpu/Dialect/CUBLAS/IR/CUBLASOps.cpp
@@ -6,10 +6,73 @@
 
 #include "openxla/compiler/nvgpu/Dialect/CUBLAS/IR/CUBLASOps.h"
 
+#include "iree/compiler/Dialect/Util/IR/UtilOps.h"
 #include "openxla/compiler/nvgpu/Dialect/CUBLAS/IR/CUBLASDialect.h"
 
 using namespace mlir;
 using namespace mlir::iree_compiler;
+
+namespace openxla::compiler::nvgpu::cublas {
+
+// Verifies that |dynamicDims| contains the appropriate number of dims for all
+// of the dynamic dimensions in |values|.
+static LogicalResult verifyOpDynamicDims(Operation *op, ValueRange values,
+                                         ValueRange dynamicDims) {
+  unsigned requiredCount = 0;
+  for (auto value : values) {
+    if (auto shapedType = value.getType().dyn_cast<ShapedType>()) {
+      requiredCount += shapedType.getNumDynamicDims();
+    }
+  }
+  if (dynamicDims.size() != requiredCount) {
+    return op->emitOpError()
+           << "value set has " << requiredCount
+           << " dynamic dimensions but only " << dynamicDims.size()
+           << " dimension values are attached";
+  }
+  return success();
+}
+
+//===----------------------------------------------------------------------===//
+// cublas.gemm operation
+//===----------------------------------------------------------------------===//
+
+ValueRange GemmOp::getOperandDynamicDims(unsigned idx) {
+  return IREE::Util::findVariadicDynamicDims(idx, getArguments(),
+                                             getArgumentDims());
+}
+ValueRange GemmOp::getResultDynamicDims(unsigned idx) {
+  return IREE::Util::findVariadicDynamicDims(idx, getResults(),
+                                             getResultDims());
+}
+
+LogicalResult GemmOp::verify() {
+  Operation *op = getOperation();
+  if (failed(verifyOpDynamicDims(op, getArguments(), getArgumentDims())) ||
+      failed(verifyOpDynamicDims(op, getResults(), getResultDims()))) {
+    return failure();
+  }
+
+  // Gemm takes `A` and `B` tensor arguments, and optional `C` tensor argument.
+  if (getNumOperands() < 2 || getNumOperands() > 3) {
+    return emitOpError() << "must have two or three arguments";
+  }
+
+  // Gemm always produces a single result.
+  if (getNumResults() != 1) {
+    return emitOpError() << "must have exactly one result";
+  }
+
+  // Only the `C` tensor argument can used as a tied operand.
+  if (auto tiedOperand = getTiedResultOperandIndex(0);
+      tiedOperand.has_value() && *tiedOperand != 2) {
+    return emitOpError() << "only third argument can be used as a tied operand";
+  }
+
+  return success();
+}
+
+}  // namespace openxla::compiler::nvgpu::cublas
 
 #define GET_OP_CLASSES
 #include "openxla/compiler/nvgpu/Dialect/CUBLAS/IR/CUBLASOps.cpp.inc"
--- compiler/src/openxla/compiler/nvgpu/Dialect/CUBLAS/IR/test/ops.mlir
+++ compiler/src/openxla/compiler/nvgpu/Dialect/CUBLAS/IR/test/ops.mlir
@@ -10,3 +10,23 @@ func.func @main(%arg0: !hal.device) -> !cublas.handle {
 // CHECK: func @main(%[[ARG0:.*]]: !hal.device) -> !cublas.handle {
 // CHECK:   cublas.handle(%[[ARG0]]) : !cublas.handle
 // CHECK: }
+
+// -----
+
+func.func @gemm(%arg0: tensor<4x4xf32>, %arg1: tensor<4x4xf32>) {
+  %0 = cublas.gemm(%arg0, %arg1) : (tensor<4x4xf32>, tensor<4x4xf32>) -> tensor<4x4xf32>
+  return
+}
+
+// CHECK: func @gemm(%[[ARG0:.*]]: tensor<4x4xf32>, %[[ARG1:.*]]: tensor<4x4xf32>)
+// CHECK:   cublas.gemm(%[[ARG0]], %[[ARG1]])
+
+// -----
+
+func.func @inplace_gemm(%arg0: tensor<4x4xf32>, %arg1: tensor<4x4xf32>, %arg2: tensor<4x4xf32>) {
+  %0 = cublas.gemm(%arg0, %arg1, %arg2) : (tensor<4x4xf32>, tensor<4x4xf32>, tensor<4x4xf32>) -> %arg2
+  return
+}
+
+// CHECK: func @inplace_gemm(%[[ARG0:.*]]: tensor<4x4xf32>, %[[ARG1:.*]]: tensor<4x4xf32>, %[[ARG2:.*]]: tensor<4x4xf32>)
+// CHECK:   cublas.gemm(%[[ARG0]], %[[ARG1]], %[[ARG2]]) {{.*}} -> %[[ARG2]]

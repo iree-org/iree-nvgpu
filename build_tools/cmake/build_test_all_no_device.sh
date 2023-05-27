d51889e8a4302c47b83cdbe8b6e8e7d62e8de8b3
Stella Laurenzo <stellaraccident@gmail.com>
2023-05-26 19:31:11 -0700

    Enable CMake based CI and align a number of ergonomics. (#108)
    * Make various components of the build optional.
    * Add convenience targets to build deps and EXCLUDE_FROM_ALL
    sub-projects (saves 20%+ cold build).
    * Add a standalone test target that can be used to run both
    device-independent and device-dependent tests.
    * Adds a CI that does a build and unit test (for device independent
    tests only).
    * Disables CUDNN runtime components for now since we need to work out
    how to get those into a docker container.

---
+++ build_tools/cmake/build_test_all_no_device.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Copyright 2019 The IREE Authors
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+# Build and run tests that do not rely on a device being present.
+
+set -xeuo pipefail
+
+SOURCE_DIR="${1:-${PWD}/openxla-nvgpu}"
+BUILD_DIR="${2:-${IREE_BUILD_DIR:-build}}"
+CMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE:-Release}"
+ENABLE_ASSERTIONS="${ENABLE_ASSERTIONS:-ON}"
+CMAKE_BIN="${CMAKE_BIN:-$(which cmake)}"
+"${CMAKE_BIN}" --version
+ninja --version
+python3 --version
+echo "Current directory: ${PWD}"
+export CTEST_PARALLEL_LEVEL="$(nproc)"
+
+declare -a CMAKE_ARGS=(
+  "-G" "Ninja"
+  "-B" "${BUILD_DIR}"
+  "-S" "${SOURCE_DIR}"
+
+  "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}"
+  "-DIREE_ENABLE_ASSERTIONS=${ENABLE_ASSERTIONS}"
+
+  # Use `lld` for faster linking.
+  "-DIREE_ENABLE_LLD=ON"
+
+  # Disable device tests.
+  "-DOPENXLA_NVGPU_INCLUDE_DEVICE_TESTS=OFF"
+
+  # Disable CUDNN runtime component for now.
+  # TODO: Figure out how to get CUDNN in the docker containers and enable.
+  "-DOPENXLA_NVGPU_BUILD_RUNTIME_CUDNN=OFF"
+)
+
+"$CMAKE_BIN" "${CMAKE_ARGS[@]}"
+
+# We first run all tests, which implicitly builds needed deps.
+# That way, we know that the test deps are declared properly.
+echo "Building openxla-nvgpu-run-tests"
+echo "--------------------------------"
+"$CMAKE_BIN" --build "${BUILD_DIR}" --target openxla-nvgpu-run-tests -- -k 0
+
+# Then followup with building all.
+echo "Building all"
+echo "------------"
+"$CMAKE_BIN" --build "${BUILD_DIR}" -- -k 0

f92a7b61cade8eda67d7d104620976213d049f60
Stella Laurenzo <stellaraccident@gmail.com>
2023-05-26 18:50:02 -0700

    Fix path

--- build_tools/cmake/build_test_all_no_device.sh
+++ build_tools/cmake/build_test_all_no_device.sh
@@ -11,16 +11,19 @@
 set -xeuo pipefail
 
 BUILD_DIR="${1:-${IREE_BUILD_DIR:-build}}"
-CMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE:-RelWithDebInfo}"
+CMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE:-Release}"
 ENABLE_ASSERTIONS="${ENABLE_ASSERTIONS:-ON}"
 CMAKE_BIN="${CMAKE_BIN:-$(which cmake)}"
 "${CMAKE_BIN}" --version
 ninja --version
 python3 --version
+echo "Current directory: ${PWD}"
+ls -l
 
 declare -a CMAKE_ARGS=(
   "-G" "Ninja"
   "-B" "${BUILD_DIR}"
+  "-S" "$PWD/openxla-nvgpu"
 
   "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}"
   "-DIREE_ENABLE_ASSERTIONS=${ENABLE_ASSERTIONS}"
